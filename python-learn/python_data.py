70567005dfe7d9dd6d5c904076f595b503e9740e
jason <591831416@qq.com>
2022-05-06 10:36:00 +0800

    ''

---
+++ python-learn/python_data.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# 1. series
+s = pd.Series(data=[1, 2, 3, 4], index=["a","b","c","d"])
+print(s)
+
+# 2. DataFrame
+df = pd.DataFrame(data=[[1, 3, 5], [2, 6, 9]], columns=["java", "python", "SQL"])
+print(df)
