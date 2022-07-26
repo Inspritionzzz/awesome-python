53520bd076e82749ec877e1d03f640ff53b4496b
jason <591831416@qq.com>
2022-07-26 23:29:38 +0800

    ''

---
+++ numpy-cn/docs/.vuepress/nav/zh.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    text: '概述',
+    link: '/about/'
+  },
+  {
+    text: '文章',
+    link: '/article/'
+  },
+  // {
+  //   text: '深度学习',
+  //   link: '/deep/',
+  //   important: true
+  // },
+  {
+    text: '用户指南',
+    link: '/user/'
+  },
+  {
+    text: '参考手册',
+    link: '/reference/'
+  },
+  {
+    text: '论坛',
+    link: 'https://www.kuxai.com/f/numpy'
+  },
+  {
+    text: '其他文档',
+    items: [
+      { text: '防脱发指南', link: '/alopecia/' },
+      { text: 'NumPy 基准测试', link: '/benchmarking/' },
+      { text: 'NumPy 开发者指南', link: '/dev/' },
+      { text: 'F2Py 指南', link: '/f2py/' },
+      { text: '构建和扩展文档', link: '/bedocs/'}
+    ]
+  },
+  {
+    text: '了解更多',
+    items: [
+      { text: '社区', link: '/community/'},
+      { text: '报告错误', link: '/bugs/' },
+      { text: '发布日志', link: 'https://numpy.org/devdocs/release.html' },
+      { text: 'NumPy 许可证', link: '/license/' },
+      { text: 'Numpy 开发计划', link: '/neps/' }
+    ]
+  }
+]
