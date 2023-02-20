7c0276b3e2821fce86a40bd9dc66b496d84387ec
zhangcy <zhangchengyu@csc.com.cn>
2023-02-20 08:47:52 +0800

    'update-20230220'

---
+++ python-learn/lucky-draw/collect_lucky_draw_info.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+import requests
+
+'''
+    1、获取更新动态
+    2、提取中奖信息动态
+'''
+def get_all_items():
+
+    url = 'https://t.bilibili.com/?spm_id_from=444.41.0.0'
+
+
+
+
+
+
+    pass
+
+if __name__ == '__main__':
+    pass
