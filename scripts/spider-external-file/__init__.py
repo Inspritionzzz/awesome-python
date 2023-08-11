a101cd54a45d44eb8c6a7625fb1c30398718d526
jason <591831416@qq.com>
2023-08-11 18:12:23 +0800

    20230811

--- scripts/spider-external-file/__init__.py
+++ scripts/spider-external-file/__init__.py
@@ -1,14 +1,18 @@
 import scrapy
 
+# 目标网站：http://www.itcast.cn/channel/teacher.shtml
 
-class MySpider(scrapy.Spider):
-    name = "example.com"
-    allowed_domains = ["example.com"]
-    start_urls = [
-        "http://www.example.com/1.html",
-        "http://www.example.com/2.html",
-        "http://www.example.com/3.html",
-    ]
+class ItcastItem(scrapy.Item):
+   name = scrapy.Field()
+   title = scrapy.Field()
+   info = scrapy.Field()
+
+class ItcastSpider(scrapy.Spider):
+    name = "itcast"
+    allowed_domains = ["itcast.cn"]
+    start_urls = (
+        'http://www.itcast.cn/',
+    )
 
     def parse(self, response):
-        self.logger.info("A response from %s just arrived!", response.url)
+        pass
